80f7160a36eeec98054d8f9e5b7e856090578f70
Aditya Kharade <adityakharade576@gmail.com>
2026-06-10 22:31:39 +0530

    Initial commit

--- Learn the basics/firstpattern.cpp
+++ Learn the basics/firstpattern.cpp
@@ -72,6 +72,68 @@ void pattern7(int n){
         cout<<endl;
     }
 }
+ void pattern10(int n){
+     
+    for (int i=0;i<=2*n-1 ;i++){
+        
+     int stars;
+
+       if (i<=n){
+        stars= i;
+       } 
+        else {
+             stars=2*n-i;
+        }
+    
+        for( int j=0;j<=stars;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+
+       }
+    }
+
+    void pattern11(int n){
+        for(int i=1;i<=n;i++){
+            int start ;
+            if(i%2==0){
+                start=0;
+            }
+            else{
+                start=1;
+
+            }
+            
+            for (int j=1;j<=i;j++){
+                cout<<start;
+                start=1-start;
+                
+            }
+            cout<<endl;
+
+        
+    }
+}
+void pattern12(int n){
+    for(int i=0;i<=n;i++){
+        //numbers;
+        for(int j=1;j<=i;j++){
+            cout<<j;
+
+        }
+        
+        for(int j=0;j<=2*(n-i);j++){
+            cout<<" ";
+        }
+
+
+        for(int j=i;j>=1;j--){
+            cout<<j;
+            
+        }
+       cout<<endl;
+    }
+}
 
     int main(){
         int t;
@@ -79,7 +141,7 @@ void pattern7(int n){
         while(t--){
             int n;
             cin>>n;
-            pattern7(n);
+            pattern12(n);
         }
 
         }
